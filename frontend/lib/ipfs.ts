d486c8e97105343f8aa6ee79eae604e98baee607
liya <jingsongroberge114@gmail.com>
2024-08-26 10:16:00 +0800

    Move Infura Project ID and Secret to Environment Variables and Update .gitignore

--- frontend/lib/ipfs.ts
+++ frontend/lib/ipfs.ts
@@ -1,7 +1,7 @@
 import { create } from 'ipfs-http-client';
 
-const projectId = 'YourInfuraProjectID'; // Replace with your Infura Project ID
-const projectSecret = 'YourInfuraProjectSecret'; // Replace with your Infura Project Secret
+const projectId = process.env.INFURA_PROJECT_ID; // Replace with your Infura Project ID
+const projectSecret = process.env.INFURA_PROJECT_SECRET; // Replace with your Infura Project Secret
 const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
 
 export const ipfsClient = create({
