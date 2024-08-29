2fdbda03df8803d353acd1b5efb25a69b1c7e463
liya <jingsongroberge114@gmail.com>
2024-08-30 02:26:26 +0800

    Refactor TopLeft component to use common goToPage function.

--- frontend/app/components/TopLeft.tsx
+++ frontend/app/components/TopLeft.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useGoToPage } from '@/utils/navigation';
 
 const TopLeft: React.FC = () => {
+  const goToPage = useGoToPage();
   return (
-    <div className="flex items-center">
+    <div className="flex items-center" onClick={() => goToPage('/')}>
       <img src="/logo.png" alt="Logo" className="logo w-12 h-12 rounded-full" />
       <span className="ml-5 ml-2 text-xl font-semibold">Social DApp Platform</span>
     </div>
