f0bc1be6467827be5d7a30796a3b0397933f762d
liya <jingsongroberge114@gmail.com>
2024-08-29 16:55:48 +0800

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,12 +4,14 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import RegisterPrompt from './components/RegisterPrompt';
 import Layout from './components/Layout';
+import RegisterPrompt from './components/RegisterPrompt';
+import { useConnect, useAccount } from "@starknet-react/core";
 
 export default function Home() {
   const router = useRouter();
   const [isPromptVisible, setIsPromptVisible] = useState(false);
+  const { address, isConnected } = useAccount();
 
   const goToPage = (page: string) => {
     router.push(page);
@@ -30,31 +32,25 @@ export default function Home() {
             <p className="text-lg text-gray-500 mirror-subtitle">
               A decentralized social media platform where users control their data and privacy.
             </p>
-
-            <div className="mt-8">        
-
-              <button
-                onClick={() => goToPage("/list")}
-                className="bg-blue-500 text-white py-2 px-4 rounded"
-              >
-                View Posts
-              </button>
-
-              <button
-                onClick={() => goToPage("/create")}
-                className="bg-blue-500 text-white py-2 px-4 rounded"
-              >
-                Create Post
-              </button>
-            </div>
-
           </div>
+          {
+            isConnected && (
+              <div className="mt-8">
+                <button
+                  onClick={() => goToPage("/list")}
+                  className="bg-blue-500 text-white py-2 px-4 rounded"
+                >
+                  View Posts
+                </button>
+              </div>
+            )
+          }
         </main>
         {isPromptVisible && (
-            <RegisterPrompt
-                onClose={() => setIsPromptVisible(false)}
-                onRegister={handleRegister}
-            />
+          <RegisterPrompt
+              onClose={() => setIsPromptVisible(false)}
+              onRegister={handleRegister}
+          />
         )}
       </div>
     </Layout>
